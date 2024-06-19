ff03a36b63a4403dfacd690f5eaee4e1a8de1c4c
Sergey Odintsov <ivanivanovplayer2019@gmail.com>
2024-06-20 00:38:56 +0200

    finish data_creation

--- Lab_1/data_creation.py
+++ Lab_1/data_creation.py
@@ -15,4 +15,51 @@ if not os.path.exists('train'):
     os.makedirs('train')
 if not os.path.exists('test'):
     os.makedirs('test')
-    
+
+np.random.seed(RANDOM_STATE)
+
+# Температура воздуха
+temperature = np.random.uniform(low=15, high=35, size=SAMPLE_SIZE)
+# Влажность воздуха
+humidity = np.random.uniform(low=0, high=100, size=SAMPLE_SIZE)
+# Независимый параметр
+noise = 50 * np.random.randn(SAMPLE_SIZE) + 200
+# Вычисляем время высыхания краски плюс шум
+drying_time = paint_drying_time(temperature, humidity) + np.random.randn(SAMPLE_SIZE) + 2
+
+# Набор данных с шумом только в целевой переменной
+df = pd.DataFrame(
+    data=np.array([temperature, humidity, noise, drying_time]).T,
+    columns=['temperature', 'humidity', 'noise', 'time']
+)
+
+train, test = train_test_split(df, test_size=0.3, random_state=RANDOM_STATE)
+train.to_csv('train/data1.csv', index=False)
+test.to_csv('test/data1.csv', index=False)
+
+# Добавим шум к температуре
+temperature = temperature + 2.2 * np.random.randn(SAMPLE_SIZE) + 5
+df = pd.DataFrame(
+    data=np.array([temperature, humidity, noise, drying_time]).T,
+    columns=['temperature', 'humidity', 'noise', 'time']
+)
+
+train, test = train_test_split(df, test_size=0.3, random_state=RANDOM_STATE)
+train.to_csv('train/data2.csv', index=False)
+test.to_csv('test/data2.csv', index=False)
+
+# Добавим шум еще к влажности
+humidity = humidity + 2 * np.random.randn(SAMPLE_SIZE) + 5
+# Добавим выбросы
+outliers = np.random.randint(low=100, high=SAMPLE_SIZE-100, size=5)
+drying_time[outliers] = drying_time[outliers] * 2
+drying_time[outliers-10] = drying_time[outliers-10] / 2
+
+df = pd.DataFrame(
+    data=np.array([temperature, humidity, noise, drying_time]).T,
+    columns=['temperature', 'humidity', 'noise', 'time']
+)
+
+train, test = train_test_split(df, test_size=0.3, random_state=RANDOM_STATE)
+train.to_csv('train/data3.csv', index=False)
+test.to_csv('test/data3.csv', index=False)

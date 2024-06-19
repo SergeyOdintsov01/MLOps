720fa83a64009fe0a2843c35f22c4bf5e039cded
Sergey Odintsov <ivanivanovplayer2019@gmail.com>
2024-06-20 01:06:09 +0200

    write correct sh script

--- Lab_1/pipeline.sh
+++ Lab_1/pipeline.sh
@@ -1,6 +1,11 @@
-!#/bin/bash
-
-python3 data_creation.py
-python3 model_preprocessing.py
-python3 model_preparation.py
-python3 model_testing.py
+#!/bin/bash
+# Установка зависимостей
+pip install -r ./requirements.txt
+# генерация данных
+python3 ./data_creation.py
+# предобработка данных
+python3 ./model_preprocessing.py
+# обучение модели
+python3 ./model_preparation.py
+# тестирование модели
+python3 ./model_testing.py

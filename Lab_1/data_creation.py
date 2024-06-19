785f214b4c279039a53e52f1f4ccba1091ec8963
Sergey Odintsov <ivanivanovplayer2019@gmail.com>
2024-06-20 00:07:21 +0200

    Add dir test, train

--- Lab_1/data_creation.py
+++ Lab_1/data_creation.py
@@ -1,5 +1,18 @@
-import os 
-import numpy as np 
+import os
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+RANDOM_STATE = 42
+SAMPLE_SIZE = 1000
+
+# Функция для моделирования
+def paint_drying_time(temperature, humidity):
+    return 24 - 0.5 * temperature + 0.2 * humidity
+
+# Создаем папки train и test, если их еще не существует
+if not os.path.exists('train'):
+    os.makedirs('train')
+if not os.path.exists('test'):
+    os.makedirs('test')
+    
